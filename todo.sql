546c484d7f475868b5d9066087c25ba6116e7b3d
Angela Vogler <angelavogler4@gmail.com>
2017-08-28 12:06:25 -0400

    finished sql document

--- todo.sql
+++ todo.sql
@@ -1,3 +1,5 @@
+
+-- Write the CREATE TABLE statement to make this table.
 CREATE TABLE todos (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
@@ -7,4 +9,21 @@ CREATE TABLE todos (
   completed_at TIMESTAMP NULL
   );
 
+-- Write INSERT statements to insert five todos into this table, with one of them completed.
 INSERT INTO todos (name, priority) VALUES ('finish weekly project', '5');
+INSERT INTO todos (name, details, priority) VALUES ('clean apartment', 'fold laundry, clean bathroom, clean kitchen, vacuum, mop', '4');
+INSERT INTO todos (name, details, priority) VALUES ('run', '3 miles Monday, Wednesday, Friday; 8 miles Saturday', '4');
+INSERT INTO todos (name, priority, completed_at) VALUES ('finish daily project', '5', NOW());
+INSERT INTO todos (name) VALUES ('watch Bachelor in Paradise');
+
+-- Write a SELECT statement to find all incomplete todos
+SELECT * FROM todos WHERE completed_at IS NULL;
+
+-- Write a SELECT statement to find all todos with a priority above 1.
+SELECT * FROM todos WHERE priority > 1;
+
+-- Write an UPDATE statement to complete one todo by its id. Your ids may differ, so you will choose the id to up.
+UPDATE todos SET completed_at = NOW() WHERE id = 2;
+
+-- Write a DELETE statement to delete all completed todo
+DELETE FROM todos WHERE completed_at IS NOT NULL;
